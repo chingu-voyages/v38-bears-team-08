4b3bbd55931c8f5ebcb5135a76f93734fc05892e
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-03-31 20:38:13 +0200

    Change port to allow Heroku to set port itself
    - Or default to 3000 for local dev

--- recipe-app-backend/index.js
+++ recipe-app-backend/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const router = require('./routes/routeIndex.js');
 const app = express();
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(
   express.urlencoded({
