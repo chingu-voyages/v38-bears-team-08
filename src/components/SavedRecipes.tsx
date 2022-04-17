9311f40b68473863affbd71607586ff3dad164ef
Dayo Akinsola <dayoakinsola3@gmail.com>
2022-04-17 01:46:42 +0100

    adding styling for saved recipes delete button

--- src/components/SavedRecipes.tsx
+++ src/components/SavedRecipes.tsx
@@ -3,7 +3,9 @@ import { useFirebaseAuth } from '../FirebaseAuthContext'
 import { auth } from '../firebase/firebaseConfig'
 import { User } from 'firebase/auth'
 import { Link } from 'react-router-dom'
-import { getAllUserRecipes } from '../firebase/firebase'
+import { getAllUserRecipes, deleteRecipe } from '../firebase/firebase'
+import { ImCross } from "react-icons/im";
+
 
 interface saveRecipeType {
   id: string
@@ -30,6 +32,12 @@ const SavedRecipes = () => {
   const user = useFirebaseAuth() || auth.currentUser
   const [userRecipes, setUserRecipes] = useState<saveRecipeType[] | []>()
 
+  const deleteSavedRecipe = async (recipeId: string) => {
+    const result = await deleteRecipe(recipeId)
+    console.log(result)
+    return result
+  }
+
   useEffect(() => {
     async function getUserRecipes() {
       if (window.sessionStorage.getItem('userRecipes')) {
@@ -54,16 +62,21 @@ const SavedRecipes = () => {
       <div id='saved-recipes'>
         {userRecipes &&
           userRecipes.map((userRecipe: saveRecipeType) => (
-            <Link key={userRecipe.id} to={`/${userRecipe.id}`} id='saved-recipe-link'>
-              <div
-                id='saved-recipe'
-                style={{
-                  background: `linear-gradient(#00000065, #00000065), url(${userRecipe.img}) center no-repeat`,
-                  backgroundSize: 'cover'
-                }}>
-                <span id='saved-recipe-title'>{userRecipe.title}</span>
+            <div id="saved-recipe-wrapper" key={userRecipe.id}>
+              <div id="delete-recipe-btn" onClick={() => deleteSavedRecipe(userRecipe.id)}>
+                <ImCross />
               </div>
-            </Link>
+              <Link  to={`/${userRecipe.id}`} id='saved-recipe-link'>
+                <div
+                  id='saved-recipe'
+                  style={{
+                    background: `linear-gradient(#00000065, #00000065), url(${userRecipe.img}) center no-repeat`,
+                    backgroundSize: 'cover'
+                  }}>
+                  <span id='saved-recipe-title'>{userRecipe.title}</span>
+                </div>
+              </Link>
+            </div>
           ))}
       </div>
     </>
